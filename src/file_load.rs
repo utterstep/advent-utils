d53d7c7fd529409526173e5f8895747a138dc525
Vlad Stepanov <8uk.8ak@gmail.com>
2021-01-08 20:41:25 +0300

    BREAKING: change parse_file/parse_raw_data typings

--- src/file_load.rs
+++ src/file_load.rs
@@ -1,13 +1,12 @@
 use std::{
     error::Error,
-    fmt::Debug,
     fs::File,
-    io::{BufReader, Read},
+    io::{self, BufReader, Read},
     path::Path,
     str::FromStr,
 };
 
-pub fn read_file<P: AsRef<Path>>(p: P) -> Result<String, Box<dyn Error>> {
+pub fn read_file<P: AsRef<Path>>(p: P) -> io::Result<String> {
     let file = File::open(p)?;
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
@@ -20,21 +19,20 @@ pub fn parse_raw_data<S, T>(data: S) -> Result<Vec<T>, Box<dyn Error>>
 where
     S: AsRef<str>,
     T: FromStr,
-    <T as FromStr>::Err: Debug,
+    <T as FromStr>::Err: Error + 'static
 {
     Ok(data
         .as_ref()
         .lines()
         .map(str::parse)
-        .map(|r| r.unwrap())
-        .collect())
+        .collect::<Result<Vec<_>, _>>()?)
 }
 
 pub fn parse_file<P, T>(p: P) -> Result<Vec<T>, Box<dyn Error>>
 where
     P: AsRef<Path>,
-    T: FromStr,
-    <T as FromStr>::Err: Debug,
+    T: FromStr<Err = Box<dyn Error>>,
+    <T as FromStr>::Err: Error + 'static,
 {
     let contents = read_file(p)?;
 
